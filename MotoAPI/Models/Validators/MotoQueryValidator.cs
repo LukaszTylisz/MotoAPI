1437727f2f3e8a14d59582f3433e12ab4f81a5da
LukaszTylisz <lukasz.tylisz@gmail.com>
2023-05-16 09:14:25 +0200

    validators

---
+++ MotoAPI/Models/Validators/MotoQueryValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using MotoAPI.Entitites;
+
+namespace MotoAPI.Models.Validators;
+
+public class MotoQueryValidator : AbstractValidator<MotoQuery>
+{
+    private int[] allowedPageSizes = new[] { 5, 10, 15 };
+
+    private string[] allowedSortByColumnNames =
+        { nameof(Moto.Name), nameof(Moto.Category), nameof(Moto.Description), };
+
+    public MotoQueryValidator()
+    {
+        RuleFor(r => r.PageNumber).GreaterThanOrEqualTo(1);
+        RuleFor(r => r.PageSize).Custom((value, context) =>
+        {
+            if (!allowedPageSizes.Contains(value))
+            {
+                context.AddFailure("PageSize", $"Pagesize must int [{string.Join(",", allowedPageSizes)}]");
+            }
+        });
+
+        RuleFor(r => r.SortBy)
+            .Must(value => string.IsNullOrEmpty(value) || allowedSortByColumnNames.Contains(value))
+            .WithMessage($"Sort by is optional, or must be in [{string.Join(",", allowedSortByColumnNames)}]");
+    }
+}
